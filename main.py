5379d0eff788d04b3509f547b995426fb7bde9ba
andndre <agungandre687@gmail.com>
2024-03-20 19:49:52 +0800

    environment variable

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from bot import TeleBot
 from handlers.arduino import add_arduino_handlers
 from handlers.t3_handler import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the API token from the environment variable
+TOKEN = os.environ.get('TOKEN')
 
 if __name__ == "__main__":        
-    tele_bot = TeleBot('6803335035:AAEsIx4P874EfbjP0OP0w9XpUVPB_0tOuoo')
+    tele_bot = TeleBot(TOKEN)
     add_arduino_handlers(tele_bot)
     add_tic_tac_toe_handlers(tele_bot)
     tele_bot.run()
