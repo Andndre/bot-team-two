f4be34175f5d16bc1fa567f874348035782491b8
cindyhps <127854850+cindyhps@users.noreply.github.com>
2024-04-26 08:40:05 +0700

    Update choose_mode.py

--- handlers/tic_tac_toe_handlers/choose_mode.py
+++ handlers/tic_tac_toe_handlers/choose_mode.py
@@ -16,19 +16,22 @@ def choose_mode(teleBot: TeleBot, query_id: int, chat_id: int, message_id: int, 
 	teleBot.bot.editMessageReplyMarkup(msg_id, reply_markup=buttons)
 
 def get_choose_mode_handler(jumlah_player: int):
-	def handler(teleBot: TeleBot, query_id, chat_id, message_id, user_id):
-		teleBot.bot.answerCallbackQuery(query_id, text='Mode: ' + str(jumlah_player) + ' player')
-		game = teleBot.get_t3_game(message_id)
-		msg_id = (chat_id, message_id)
-		if jumlah_player == 1:
-			game.set_symbol_player_count(1)
-			size_buttons(teleBot, query_id, chat_id, message_id, user_id)
-		if jumlah_player == 2:
-			game.set_symbol_player_count(2)
-			teleBot.bot.editMessageText(msg_id, 'Tag lawan Anda!')
-			teleBot.bot.editMessageReplyMarkup(msg_id, reply_markup=None)
-		if jumlah_player == 3:
-			game.set_symbol_player_count(3)
-			# TODO: tampilan untuk multi player		
+    def handler(teleBot: TeleBot, query_id, chat_id, message_id, user_id):
+        teleBot.bot.answerCallbackQuery(query_id, text='Mode: ' + str(jumlah_player) + ' player')
+        game = teleBot.get_t3_game(message_id)
+        msg_id = (chat_id, message_id)
+        if jumlah_player == 1:
+            game.set_symbol_player_count(1)
+            size_buttons(teleBot, query_id, chat_id, message_id, user_id)
+        if jumlah_player == 2:
+            game.set_symbol_player_count(2)
+            teleBot.bot.editMessageText(msg_id, 'Tag lawan Anda!')
+            teleBot.bot.editMessageReplyMarkup(msg_id, reply_markup=None)
+        if jumlah_player == 3:
+            game.set_symbol_player_count(3)
+            teleBot.bot.editMessageText(msg_id, 'Menunggu pemain ketiga untuk bergabung...', 
+                                        reply_markup=InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Bergabung', callback_data='join')]]))
+            # Menghapus tombol mode setelah memulai permainan
+            teleBot.bot.editMessageReplyMarkup(msg_id, reply_markup=None)	
 		
 	return handler
